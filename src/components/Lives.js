641eb5b0d66cb66ed4501a30564b3a4f7ab2639a
Tran Ho Ngoc Huyen <tranho@Tran.local>
2024-11-18 15:51:32 +0700

    update Lives

--- src/components/Lives.js
+++ src/components/Lives.js
@@ -3,14 +3,14 @@ import React from 'react';
 function Lives({ wrongClicks }) {
   return (
     <div className="lives-container">
-  <span>Lives:</span>
-  <div className="lives-bar">
-    <div
-      className="lives-bar-inner"
-      style={{ width: `${((3 - wrongClicks) / 3) * 100}%` }}
-    ></div>
-  </div>
-</div>
+      <span>Lives:</span>
+        <div className="lives-bar">
+          <div
+            className="lives-bar-inner"
+            style={{ width: `${((3 - wrongClicks) / 3) * 100}%` }}
+          ></div>
+        </div>
+    </div>
 
   );
 }
